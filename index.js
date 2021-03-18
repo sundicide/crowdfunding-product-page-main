d26427a991323f3af325d8920562bb5c5a9e5262
sundicide <kyusuen@gmail.com>
2021-03-19 08:31:53 +0900

    fix: dynamic rendering on cards with jQuery

--- index.js
+++ index.js
@@ -33,3 +33,52 @@ function closeModal() {
   var modal = document.querySelector("div[class*='modal']");
   modal.classList.remove("active");
 }
+
+$(document).ready(() => {
+  const proejctCards = [
+    {
+      contents: " You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+      left: 101,
+      pledge: 25,
+      title: "Bamboo Stand"
+    },
+    {
+      contents: " You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+      left: 64,
+      pledge: 75,
+      title: "Black Edition Stand"
+    },
+    {
+      contents: " You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+      left: 0,
+      pledge: 200,
+      title: "Mahogany Special Edition"
+    }
+  ];
+
+  const targetDom = $("div[class='dynamic-cards']");
+  proejctCards.forEach((d) => {
+    targetDom.append(`<div>
+      <div class="card">
+        <div class="card-top">
+          <p class="subtitle">${d.title}</p>
+          <a href="#">Pledge $${d.pledge} or more</a>
+        </div>
+        <p class="card-middle">${d.contents}</p>
+        <div class="card-bottom">
+          <div>
+            <span class="card-bottom__value">${d.left}</span>
+            <span class="card-bottom__postfix">left</span>
+          </div>
+          ${(d.left === 0) ?
+            `<div> Out of Stock </div>`
+              :
+            `<div>
+              <button onclick="selectReward()"> Select Reward </button>
+            </div>`
+          }
+        </div>
+      </div>
+    </div>`);
+  });
+});
